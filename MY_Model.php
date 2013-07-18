6fccad2310f632dcd516113f04af3755e6482ef7
Roni Saha <roni.cse@gmail.com>
2013-07-18 15:25:11 +0600

    New Method added(remove_in, on_duplicate_update)

--- MY_Model.php
+++ MY_Model.php
@@ -1,4 +1,5 @@
-<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+
 
 /**
  * MY_Model
@@ -7,21 +8,22 @@
  * in writing very small amount of code to do general tasks that
  * are needed in an application.
  *
- * @author Md Emran Hasan <phpfour@gmail.com>
- * @author Roni Kumar Saha <roni.cse@gmail.com>
+ * @author  Md Emran Hasan <phpfour@gmail.com>
+ * @author  Roni Kumar Saha <roni.cse@gmail.com>
  * @version 1.4
- * @since 2012
+ * @since   2012
  */
+
 class MY_Model extends CI_Model
 {
     /**
      * @var string name of table
      */
-    protected $table=NULL;
+    protected $table = NULL;
     /**
      * @var string the primary key
      */
-    protected $primaryKey=NULL;
+    protected $primaryKey = NULL;
 
     /**
      * @var array list of fields
@@ -50,7 +52,7 @@ class MY_Model extends CI_Model
     public function __construct()
     {
         parent::__construct();
-        ($this->table!=NULL) AND $this->loadTable($this->table,$this->primaryKey);
+        ($this->table != NULL) AND $this->loadTable($this->table, $this->primaryKey);
     }
 
     /**
@@ -59,31 +61,31 @@ class MY_Model extends CI_Model
      */
     public function loadTable($table, $primaryKey = 'id')
     {
-        $this->table = $table;
+        $this->table      = $table;
         $this->primaryKey = $primaryKey;
-        $this->fields = $this->db->list_fields($table);
+        $this->fields     = $this->db->list_fields($table);
     }
 
     /**
-     * @param null|string|array     $conditions
-     * @param string                $fields
-     * @param null|string|array     $order
-     * @param int                   $start
-     * @param null|int              $limit
+     * @param null|string|array $conditions
+     * @param string            $fields
+     * @param null|string|array $order
+     * @param int               $start
+     * @param null|int          $limit
      *
      * @return mixed
      */
     public function findAll($conditions = NULL, $fields = '*', $order = NULL, $start = 0, $limit = NULL)
     {
-        if ($conditions != NULL)  {
-            if(is_array($conditions)) {
+        if ($conditions != NULL) {
+            if (is_array($conditions)) {
                 $this->db->where($conditions);
             } else {
                 $this->db->where($conditions, NULL, FALSE);
             }
         }
 
-        if ($fields != NULL)  {
+        if ($fields != NULL) {
             $this->db->select($fields);
         }
 
@@ -91,7 +93,7 @@ class MY_Model extends CI_Model
             $this->order_by($order);
         }
 
-        if ($limit != NULL)  {
+        if ($limit != NULL) {
             $this->db->limit($limit, $start);
         }
 
@@ -105,16 +107,18 @@ class MY_Model extends CI_Model
      *
      * @return mixed(array|object) based on the returnArray value
      */
-    public function getResult(){
-        $query = $this->db->get($this->table);
+    public function getResult()
+    {
+        $query         = $this->db->get($this->table);
         $this->numRows = $query->num_rows();
         return ($this->returnArray) ? $query->result_array() : $query->result();
     }
 
     /**
-     * @param null|string|array     $conditions
-     * @param string                $fields
-     * @param null|string|array     $order
+     * @param null|string|array $conditions
+     * @param string            $fields
+     * @param null|string|array $order
+     *
      * @return bool
      */
     public function find($conditions = NULL, $fields = '*', $order = NULL)
@@ -123,7 +127,7 @@ class MY_Model extends CI_Model
 
         if ($data) {
             return $data[0];
-        } else  {
+        } else {
             return FALSE;
         }
     }
@@ -133,6 +137,7 @@ class MY_Model extends CI_Model
      * @param        $name
      * @param string $fields
      * @param null   $order
+     *
      * @return bool
      */
     public function field($conditions = NULL, $name, $fields = '*', $order = NULL)
@@ -151,6 +156,7 @@ class MY_Model extends CI_Model
 
     /**
      * @param null $conditions
+     *
      * @return bool
      */
     public function findCount($conditions = NULL)
@@ -166,6 +172,7 @@ class MY_Model extends CI_Model
 
     /**
      * @param null $data
+     *
      * @return bool|null
      */
     public function insert($data = NULL)
@@ -190,13 +197,14 @@ class MY_Model extends CI_Model
      * @param null $data
      * @param null $id
      * @param null $conditions
+     *
      * @return bool|null
      */
-    public function update($data = NULL, $id = NULL,$conditions=NULL)
+    public function update($data = NULL, $id = NULL, $conditions = NULL)
     {
-        $this->affectedRows=NULL;
+        $this->affectedRows = NULL;
 
-        if($id==NULL && $conditions==NULL && $data!=NULL){  //THis is an insert operation
+        if ($id == NULL && $conditions == NULL && $data != NULL) { //THis is an insert operation
             return $this->insert($data);
         }
 
@@ -214,39 +222,114 @@ class MY_Model extends CI_Model
             $this->db->where($this->primaryKey, $id);
             $this->db->update($this->table, $data);
             $this->affectedRows = $this->db->affected_rows();
-        } elseif($conditions!=NULL){
+        } elseif ($conditions != NULL) {
             $this->db->where($conditions);
             $this->db->update($this->table, $data);
             $this->affectedRows = $this->db->affected_rows();
         }
+
         return $id;
+    }
+
+    /**
+     * @param null $data
+     * @param null $update
+     *
+     * @return bool
+     */
+    public function on_duplicate_update($data = NULL, $update = NULL)
+    {
+        if (is_null($data)) {
+            return FALSE;
+        }
+
+        $sql = $this->_duplicate_insert_sql($this->db->_protect_identifiers($this->table), $data, $update);
+        return $this->db->query($sql);
+    }
+
+    /**
+     * @param      $table
+     * @param      $values
+     * @param null $update
+     *
+     * @return string
+     */
+    private function _duplicate_insert_sql($table, $values, $update = NULL)
+    {
+        $updateStr = array();
+        $keyStr    = array();
+        $valStr    = array();
+
+        foreach ($values as $key => $val) {
+            $keyStr[] = $key;
+            $valStr[] = $this->db->escape($val);
+        }
+
+        if (is_null($update)) {
+            $update = $values;
+        }
+
+        foreach ($update as $key => $val) {
+            $updateStr[] = $key . " = '{$val}'";
+        }
+
+        $sql = "INSERT INTO " . $table . " (" . implode(', ', $keyStr) . ") ";
+        $sql .= "VALUES (" . implode(', ', $valStr) . ") ";
+        $sql .= "ON DUPLICATE KEY UPDATE " . implode(", ", $updateStr);
+
+        return $sql;
     }
 
     /**
      * @param null $id
      * @param null $conditions
+     *
      * @return bool
      */
-    public function remove($id = NULL,$conditions=NULL)
+    public function remove($id = NULL, $conditions = NULL)
     {
-        if ($id != NULL){
+        if ($id != NULL) {
             $this->db->where($this->primaryKey, $id);
-        }elseif($conditions!=NULL) {
+        } elseif ($conditions != NULL) {
             $this->db->where($conditions);
-        }else{
+        } else {
             return FALSE;
         }
         return $this->db->delete($this->table);
     }
 
+
+    /**
+     * @param null $values
+     * @param null $field
+     *
+     * @return bool
+     */
+    public function remove_in($values = NULL, $field = NULL)
+    {
+        if ($values === NULL) {
+            return FALSE;
+        }
+
+        if ($field === NULL) {
+            $field = $this->primaryKey;
+        }
+
+        $this->db->where_in($field, $values);
+
+        return $this->db->delete($this->table);
+    }
+
+
     /**
      * @param $method
      * @param $args
+     *
      * @return mixed
      */
-    public function __call ($method, $args)
+    public function __call($method, $args)
     {
-        $watch = array('findBy','findAllBy','findFieldBy');
+        $watch = array('findBy', 'findAllBy', 'findFieldBy');
 
         foreach ($watch as $found) {
             if (stristr($method, $found)) {
@@ -263,23 +346,25 @@ class MY_Model extends CI_Model
      * <pre>
      * $value=$model->propertyName;
      * </pre>
+     *
      * @param string $name the property name
+     *
      * @return mixed the property value
      * @see __set
      */
     public function __get($name)
     {
-        $getter='get'.$name;
-        if(method_exists($this,$getter))
+        $getter = 'get' . $name;
+        if (method_exists($this, $getter))
             return $this->$getter();
-        else if(isset($this->$name))
+        else if (isset($this->$name))
             return $this->$name;
 
         $trace = debug_backtrace();
         trigger_error(
             'Undefined property : ' . $name .
-                ' in ' . $trace[0]['file'] .
-                ' on line ' . $trace[0]['line'],
+            ' in ' . $trace[0]['file'] .
+            ' on line ' . $trace[0]['line'],
             E_USER_NOTICE);
         return NULL;
     }
@@ -290,20 +375,21 @@ class MY_Model extends CI_Model
      * @param        $value
      * @param string $fields
      * @param null   $order
+     *
      * @return bool
      */
-    public function findBy($field, $value,$fields='*',$order=NULL)
+    public function findBy($field, $value, $fields = '*', $order = NULL)
     {
-        $arg_list=array();
-        if(is_array($value)){
-            $arg_list=$value;
-            $value=$arg_list[0];
+        $arg_list = array();
+        if (is_array($value)) {
+            $arg_list = $value;
+            $value    = $arg_list[0];
         }
-        $fields = isset($arg_list[1])?$arg_list[1]:$fields;
-        $order = isset($arg_list[2])?$arg_list[2]:$order;
+        $fields = isset($arg_list[1]) ? $arg_list[1] : $fields;
+        $order  = isset($arg_list[2]) ? $arg_list[2] : $order;
 
         $where = array($field => $value);
-        return $this->find($where,$fields,$order);
+        return $this->find($where, $fields, $order);
     }
 
     /**
@@ -313,22 +399,23 @@ class MY_Model extends CI_Model
      * @param null   $order
      * @param int    $start
      * @param null   $limit
+     *
      * @return mixed
      */
-    public function findAllBy($field, $value, $fields='*',$order=NULL,$start=0,$limit=NULL)
+    public function findAllBy($field, $value, $fields = '*', $order = NULL, $start = 0, $limit = NULL)
     {
-        $arg_list=array();
-        if(is_array($value)){
-            $arg_list=$value;
-            $value=$arg_list[0];
+        $arg_list = array();
+        if (is_array($value)) {
+            $arg_list = $value;
+            $value    = $arg_list[0];
         }
-        $fields = isset($arg_list[1])?$arg_list[1]:$fields;
-        $order = isset($arg_list[2])?$arg_list[2]:$order;
-        $start = isset($arg_list[3])?$arg_list[3]:$start;
-        $limit = isset($arg_list[4])?$arg_list[4]:$limit;
+        $fields = isset($arg_list[1]) ? $arg_list[1] : $fields;
+        $order  = isset($arg_list[2]) ? $arg_list[2] : $order;
+        $start  = isset($arg_list[3]) ? $arg_list[3] : $start;
+        $limit  = isset($arg_list[4]) ? $arg_list[4] : $limit;
 
         $where = array($field => $value);
-        return $this->findAll($where,$fields,$order,$start,$limit);
+        return $this->findAll($where, $fields, $order, $start, $limit);
     }
 
     /**
@@ -337,17 +424,19 @@ class MY_Model extends CI_Model
      * @param        $value
      * @param string $fields
      * @param null   $order
+     *
      * @return mixed
      */
-    public function findFieldBy($field, $value,$fields='*',$order=NULL){
-        $arg_list=array();
-        if(is_array($value)){
-            $arg_list=$value;
-            $value=$arg_list[0];
+    public function findFieldBy($field, $value, $fields = '*', $order = NULL)
+    {
+        $arg_list = array();
+        if (is_array($value)) {
+            $arg_list = $value;
+            $value    = $arg_list[0];
         }
-        $fields = isset($arg_list[1])?$arg_list[1]:$fields;
-        $order = isset($arg_list[2])?$arg_list[2]:$order;
-        $where = array($field => $value);
+        $fields = isset($arg_list[1]) ? $arg_list[1] : $fields;
+        $order  = isset($arg_list[2]) ? $arg_list[2] : $order;
+        $where  = array($field => $value);
         return $this->field($where, $fields, $fields, $order);
     }
 
@@ -362,32 +451,38 @@ class MY_Model extends CI_Model
      * or
      * $model->orderby(array(array('fieldname','DESC'),'fieldname DESC'));
      * </pre>
+     *
      * @param mixed(string|array) $orders
      *
      * @return bool
      */
-    public function order_by($orders=NULL){
+    public function order_by($orders = NULL)
+    {
         if ($orders == NULL) {
             return FALSE;
         }
-        if(is_array($orders)){   //Multiple order by provided!
+
+        if (is_array($orders)) { //Multiple order by provided!
             //check if we got single order by passed as array!!
-            if(isset($orders[1]) && (strtolower($orders[1])=='asc' || strtolower($orders[1])=='desc' || strtolower($orders[1]) == 'random')){
-                $this->db->order_by($orders[0],$orders[1]);
+            if (isset($orders[1]) && (strtolower($orders[1]) == 'asc' || strtolower($orders[1]) == 'desc' || strtolower($orders[1]) == 'random')) {
+                $this->db->order_by($orders[0], $orders[1]);
                 return;
             }
-            foreach($orders as $order){
+            foreach ($orders as $order) {
                 $this->order_by($order);
             }
             return;
         }
+
         $this->db->order_by($orders); //its a string just call db order_by
+
         return TRUE;
     }
 
 
     /**
      * @param $sql
+     *
      * @return mixed
      */
     public function executeQuery($sql)
@@ -405,6 +500,7 @@ class MY_Model extends CI_Model
 
     /**
      * @param $data
+     *
      * @return mixed
      */
     public function getInsertString($data)
